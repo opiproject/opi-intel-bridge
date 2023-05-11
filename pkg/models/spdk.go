602269addb78c68a3ce62b74c30ecf637e7ce200
Artsiom Koltun <artsiom.koltun@intel.com>
2023-05-11 09:20:49 +0200

    Add SPDK rpc structures to set QoS to controller.
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- pkg/models/spdk.go
+++ pkg/models/spdk.go
@@ -23,3 +23,17 @@ type NpiBdevClearKeysParams struct {
 
 // NpiBdevClearKeysResult is the result of clearing crypto keys
 type NpiBdevClearKeysResult bool
+
+// NpiQosBwIopsLimitParams holds the parameters required to set QoS limits
+type NpiQosBwIopsLimitParams struct {
+	Nqn          string `json:"nqn"`
+	MaxReadIops  int    `json:"max_read_iops"`
+	MaxWriteIops int    `json:"max_write_iops"`
+	MaxReadBw    int    `json:"max_read_bw"`
+	MaxWriteBw   int    `json:"max_write_bw"`
+	MinReadBw    int    `json:"min_read_bw"`
+	MinWriteBw   int    `json:"min_write_bw"`
+}
+
+// NpiQosBwIopsLimitResult is the result of setting QoS limits
+type NpiQosBwIopsLimitResult bool
