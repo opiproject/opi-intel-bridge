0c6ed2183a5f3a29c7b5805cd9300543b372b0cf
Artsiom Koltun <artsiom.koltun@intel.com>
2023-05-11 09:36:44 +0200

    Add test infrastructure to run stub SPDK service.
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

---
+++ pkg/frontend/frontend_test.go
@@ -0,0 +1,81 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (C) 2023 Intel Corporation
+
+// Package frontend implements the FrontEnd APIs (host facing) of the storage Server
+package frontend
+
+import (
+	"context"
+	"log"
+	"net"
+	"os"
+
+	"github.com/opiproject/gospdk/spdk"
+	pb "github.com/opiproject/opi-api/storage/v1alpha1/gen/go"
+	"github.com/opiproject/opi-spdk-bridge/pkg/server"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+	"google.golang.org/grpc/test/bufconn"
+)
+
+type frontendClient struct {
+	pb.FrontendNvmeServiceClient
+}
+
+type testEnv struct {
+	opiSpdkServer *Server
+	client        *frontendClient
+	ln            net.Listener
+	testSocket    string
+	ctx           context.Context
+	conn          *grpc.ClientConn
+	jsonRPC       spdk.JSONRPC
+}
+
+func (e *testEnv) Close() {
+	server.CloseListener(e.ln)
+	if err := os.RemoveAll(e.testSocket); err != nil {
+		log.Fatal(err)
+	}
+	server.CloseGrpcConnection(e.conn)
+}
+
+func createTestEnvironment(startSpdkServer bool, spdkResponses []string) *testEnv {
+	env := &testEnv{}
+	env.testSocket = server.GenerateSocketName("frontend")
+	env.ln, env.jsonRPC = server.CreateTestSpdkServer(env.testSocket, startSpdkServer, spdkResponses)
+	env.opiSpdkServer = NewServer(env.jsonRPC)
+
+	ctx := context.Background()
+	conn, err := grpc.DialContext(ctx,
+		"",
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithContextDialer(dialer(env.opiSpdkServer)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	env.ctx = ctx
+	env.conn = conn
+
+	env.client = &frontendClient{
+		pb.NewFrontendNvmeServiceClient(env.conn),
+	}
+
+	return env
+}
+
+func dialer(opiSpdkServer *Server) func(context.Context, string) (net.Conn, error) {
+	listener := bufconn.Listen(1024 * 1024)
+	server := grpc.NewServer()
+	pb.RegisterFrontendNvmeServiceServer(server, opiSpdkServer)
+
+	go func() {
+		if err := server.Serve(listener); err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	return func(context.Context, string) (net.Conn, error) {
+		return listener.Dial()
+	}
+}
