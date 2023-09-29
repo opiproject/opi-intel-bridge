e31be295964a5b9fdb54575fcf5d02d94f20965d
Artsiom Koltun <artsiom.koltun@intel.com>
2023-09-29 09:11:31 +0200

    feat: add grpc interceptor to log calls
    Signed-off-by: Artsiom Koltun <artsiom.koltun@intel.com>

--- cmd/main.go
+++ cmd/main.go
@@ -34,6 +34,7 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 	"google.golang.org/grpc/reflection"
 
+	"github.com/grpc-ecosystem/go-grpc-middleware/v2/interceptors/logging"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 )
 
@@ -90,6 +91,16 @@ func runGrpcServer(grpcPort int, spdkAddress string, tlsFiles string, store gokv
 		}
 		serverOptions = append(serverOptions, option)
 	}
+	serverOptions = append(serverOptions, grpc.UnaryInterceptor(
+		logging.UnaryServerInterceptor(utils.InterceptorLogger(log.Default()),
+			logging.WithLogOnEvents(
+				logging.StartCall,
+				logging.FinishCall,
+				logging.PayloadReceived,
+				logging.PayloadSent,
+			),
+		)),
+	)
 	s := grpc.NewServer(serverOptions...)
 
 	jsonRPC := spdk.NewSpdkJSONRPC(spdkAddress)
