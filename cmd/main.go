d89908bc088402be506848aff0c50d8ad699caf5
Moshe Shahar <mooshons@gmail.com>
2024-04-18 11:05:00 +0300

    feat: add malloc volume service
    Signed-off-by: Moshe Shahar <mooshons@gmail.com>

--- cmd/main.go
+++ cmd/main.go
@@ -133,6 +133,7 @@ func runGrpcServer(grpcPort int, spdkAddress string, tlsFiles string, store gokv
 	pb.RegisterFrontendVirtioScsiServiceServer(s, frontendOpiSpdkServer)
 	pb.RegisterNvmeRemoteControllerServiceServer(s, backendOpiSpdkServer)
 	pb.RegisterNullVolumeServiceServer(s, backendOpiSpdkServer)
+	pb.RegisterMallocVolumeServiceServer(s, backendOpiSpdkServer)
 	pb.RegisterAioVolumeServiceServer(s, backendOpiSpdkServer)
 	pb.RegisterMiddleendEncryptionServiceServer(s, middleendOpiIntelServer)
 	pb.RegisterMiddleendQosVolumeServiceServer(s, middleendOpiIntelServer)
@@ -161,6 +162,7 @@ func runGatewayServer(grpcPort int, httpPort int) {
 
 	registerGatewayHandler(ctx, mux, endpoint, opts, pb.RegisterAioVolumeServiceHandlerFromEndpoint, "backend aio")
 	registerGatewayHandler(ctx, mux, endpoint, opts, pb.RegisterNullVolumeServiceHandlerFromEndpoint, "backend null")
+	registerGatewayHandler(ctx, mux, endpoint, opts, pb.RegisterMallocVolumeServiceHandlerFromEndpoint, "backend malloc")
 	registerGatewayHandler(ctx, mux, endpoint, opts, pb.RegisterNvmeRemoteControllerServiceHandlerFromEndpoint, "backend nvme")
 
 	registerGatewayHandler(ctx, mux, endpoint, opts, pb.RegisterMiddleendEncryptionServiceHandlerFromEndpoint, "middleend encryption")
